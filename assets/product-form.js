c8ddf5ed0b1d833fb386bc7e5f7649408280567e
S-Arturs <88853777+S-Arturs@users.noreply.github.com>
2025-08-26 16:07:53 +0300

    TASK-3

--- assets/product-form.js
+++ assets/product-form.js
@@ -36,8 +36,10 @@ if (!customElements.get('product-form')) {
       }
     
       //Added to generate selling plan add to cart response
-      const sellingPlanId = window.getCurrentSellingPlanId();
-      formData.append("selling_plan", sellingPlanId);
+      const sellingPlanId = window.getCurrentSellingPlanId ? window.getCurrentSellingPlanId() : null;
+      if (sellingPlanId) {
+        formData.append("selling_plan", sellingPlanId);
+      }
       
       config.body = formData;
 
